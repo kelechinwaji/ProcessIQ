879d0ab8c98ce50ac68fa941adf17fdcac191a8c
kelechinwaji <khayceesdike@gmail.com>
2024-12-30 17:46:20 +0100

    feat: google oauth strategy service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerService } from './common/logger/logger.service';
+import { OauthService } from './auth/oauth/oauth.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, LoggerService],
+  providers: [AppService, LoggerService, OauthService],
 })
 export class AppModule {}
---
+++ src/auth/oauth/oauth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-oauth2';
+
+@Injectable()
+export class GoogleOAuthStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor() {
+    super({
+      authorizationURL: 'https://provider.com/oauth/authorize',
+      tokenURL: 'https://provider.com/oauth/token',
+      clientID: process.env.OAUTH_CLIENT_ID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/callback',
+      scope: ['profile', 'email'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+  ): Promise<any> {
+    // Handle user validation or save user info in DB
+    return { accessToken, profile };
+  }
+}
