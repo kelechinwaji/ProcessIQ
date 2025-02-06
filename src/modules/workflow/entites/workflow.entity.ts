d9bc5d4f09e619537ed65cc433e9918588b4f07f
kelechinwaji <khayceesdike@gmail.com>
2025-02-06 06:11:58 +0100

    feat: workflow entity

---
+++ src/modules/workflow/entites/workflow.entity.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export enum WorkflowStatus {
+  ACTIVE = 'active',
+  PAUSED = 'paused',
+  COMPLETED = 'completed',
+}
+
+@Schema({ timestamps: true }) // Adds createdAt and updatedAt fields
+export class Workflow extends Document {
+  @Prop({ required: true, unique: true })
+  id: string;
+
+  @Prop({ required: true })
+  user: string;
+
+  @Prop({ required: true, unique: true })
+  name: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({
+    required: true,
+    enum: WorkflowStatus,
+    default: WorkflowStatus.ACTIVE,
+  })
+  status: WorkflowStatus;
+}
+
+export const WorkflowSchema = SchemaFactory.createForClass(Workflow);
